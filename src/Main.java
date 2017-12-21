668f46fbc09726e8deff688e57a230e04ef1946c
Victor Wissink desktop <victor-wissink@hotmail.com>
2017-12-21 23:41:15 +0100

    initial project

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.io.File;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String path = args[0];
+
+
+
+
+
+
+
+        File CP_file = new File(path);
+
+
+    }
+}
